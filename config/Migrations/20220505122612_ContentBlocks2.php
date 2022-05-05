5b3e799e39346b864f532cc3f7d6a05a21b1df90
Pascal Schneider <psc@brandcom.de>
2022-05-05 12:27:31 +0200

    migration blocks: is_published and html_anchor

---
+++ config/Migrations/20220505122612_ContentBlocks2.php
@@ -0,0 +1,32 @@
+<?php
+
+use Migrations\AbstractMigration;
+
+class ContentBlocks2 extends AbstractMigration
+{
+    public $autoId = false;
+
+    public function up()
+    {
+        $this->table('content_blocks_blocks')
+            ->addColumn('is_published', 'boolean', [
+                'default' => false,
+                'limit' => null,
+                'null' => false,
+            ])
+            ->addColumn('html_anchor', 'string', [
+                'default' => null,
+                'limit' => 255,
+                'null' => true,
+            ])
+            ->create();
+    }
+
+    public function down()
+    {
+        $this->table('content_blocks_blocks')
+            ->removeColumn('is_published')
+            ->removeColumn('html_anchor')
+            ->update();
+    }
+}
